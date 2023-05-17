243f331021502f010091678d2e8b0229fe7b6088
Borjan <crvenkov_borjan@yahoo.com>
2023-05-17 18:51:21 +0200

    Project set up

---
+++ src/main/java/uikt/uiktpteamretrobnd/UiktpTeamRetroBndApplication.java
@@ -0,0 +1,13 @@
+package uikt.uiktpteamretrobnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UiktpTeamRetroBndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UiktpTeamRetroBndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uikt/uiktpteamretrobnd/UiktpTeamRetroBndApplicationTests.java
@@ -0,0 +1,13 @@
+package uikt.uiktpteamretrobnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UiktpTeamRetroBndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
