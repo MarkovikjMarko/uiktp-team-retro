2923f147ac17a57d0a80d6ccbc5670baaab6b58c
Borjan <crvenkov_borjan@yahoo.com>
2023-05-22 20:16:37 +0200

    Improvements to the item service

--- src/main/java/uikt/uiktpteamretrobnd/service/ItemService.java
+++ src/main/java/uikt/uiktpteamretrobnd/service/ItemService.java
@@ -34,7 +34,9 @@ public class ItemService {
     public Item create(ItemRequest itemRequest) {
         Category category = this.categoryRepository.findById(itemRequest.getCategoryId()).orElseThrow(ModelNotFoundException::new);
 
-        Item item = new Item(itemRequest.getBody(), category);
+        String body = itemRequest.getBody();
+
+        Item item = new Item(body, category);
 
         return this.itemRepository.save(item);
     }
@@ -42,12 +44,15 @@ public class ItemService {
     public Item update(Long id, ItemRequest itemRequest) {
         Item item = itemRepository.findById(id).orElseThrow(ModelNotFoundException::new);
 
-        if(itemRequest.getBody() != null){
-            item.setBody(itemRequest.getBody());
+        String body = itemRequest.getBody();
+        Long categoryId = itemRequest.getCategoryId();
+
+        if(body != null){
+            item.setBody(body);
         }
 
-        if(itemRequest.getCategoryId() != null){
-            Category category = this.categoryRepository.findById(itemRequest.getCategoryId()).orElseThrow(ModelNotFoundException::new);
+        if(categoryId != null){
+            Category category = this.categoryRepository.findById(categoryId).orElseThrow(ModelNotFoundException::new);
             item.setCategory(category);
         }
 
